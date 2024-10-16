598dcef5109a0f53b8b5cdc104633e55fbbf9348
Gianni <117100331+SW364@users.noreply.github.com>
2024-10-15 22:59:11 -0500

    Update Navigation.kt

--- app/src/main/java/com/example/buffetec/Navigation/Navigation.kt
+++ app/src/main/java/com/example/buffetec/Navigation/Navigation.kt
@@ -4,5 +4,6 @@ sealed class Screen (val route: String){
     object Login : Screen("Login")
     object Signup: Screen("Signup")
     object  Profile : Screen("Profile")
+    object Biblioteca : Screen("Biblioteca")
     object Cases : Screen("Cases")
 }
